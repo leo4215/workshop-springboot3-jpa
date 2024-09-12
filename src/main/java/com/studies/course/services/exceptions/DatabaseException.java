cd1ecaa50eff4f5ecc4ec1de938f028d558780fc
Leonardo de Camargo <leonardo4215@live.com>
2024-09-12 11:10:05 -0400

    Exception handling - delete

---
+++ src/main/java/com/studies/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.studies.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
